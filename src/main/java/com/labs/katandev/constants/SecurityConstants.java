e16ecced0ebdf5773f100de22e83b5e05d7b22fa
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-03 00:08:07 +0700

    Remove unused code.

--- src/main/java/com/labs/katandev/constants/SecurityConstants.java
+++ src/main/java/com/labs/katandev/constants/SecurityConstants.java
@@ -1,7 +1,6 @@
 package com.labs.katandev.constants;
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 432_000_000; // 5 days expressed in milliseconds
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String JWT_TOKEN_HEADER = "Jwt-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
