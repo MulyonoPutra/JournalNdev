bf05203cf00a2cdc0f3e3d8b5f1f77a25aaf9b98
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-03 10:38:24 +0700

    Post & Category: Implements GET, DELETE, POST.

--- src/main/java/com/labs/katandev/controllers/CategoryController.java
+++ src/main/java/com/labs/katandev/controllers/CategoryController.java
@@ -1,10 +1,11 @@
 package com.labs.katandev.controllers;
 
 import com.labs.katandev.domain.dto.CategoryDTO;
+import com.labs.katandev.domain.dto.ResponseMessages;
 import com.labs.katandev.domain.entity.Category;
 import com.labs.katandev.exception.BadException;
-import com.labs.katandev.repository.CategoryRepository;
 import com.labs.katandev.service.CategoryService;
+import com.labs.katandev.utils.ResponseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,8 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+import static com.labs.katandev.constants.ResponseConstants.DELETED_SUCCESSFULLY;
+
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
@@ -23,16 +26,21 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    private final CategoryRepository categoryRepository;
-
-    public CategoryController(CategoryService categoryService, CategoryRepository categoryRepository) {
+    public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
-        this.categoryRepository = categoryRepository;
     }
 
+
+    /**
+     * {@code POST  /category} : Create a new category.
+     *
+     * @param categoryDTO the categoryDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new categoryDTO, or with status {@code 400 (Bad Request)} if the category has already an ID.
+     * @throws BadException if the Location URI syntax is incorrect.
+     */
     @PostMapping
     public ResponseEntity<CategoryDTO> createPost(@Valid @RequestBody CategoryDTO categoryDTO) throws BadException {
-        log.debug("REST request to save Post : {}", categoryDTO);
+        log.debug("REST request to save Category : {}", categoryDTO);
         if (categoryDTO.getId() != null) {
             throw new BadException("Error!");
         }
@@ -40,16 +48,44 @@ public class CategoryController {
         return ResponseEntity.ok(result);
     }
 
+
+    /**
+     * {@code GET  /category} : get all the categories.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of categories in body.
+     */
     @GetMapping
     public ResponseEntity<List<Category>> findAllCategory() {
         List<Category> categories = categoryService.findAll();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
+
+    /**
+     * {@code GET  /category/:id} : get the "id" category.
+     *
+     * @param id the id of the categoryDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the categoryDTO, or with status {@code 404 (Not Found)}.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         log.debug("REST request to get Category : {}", id);
         Optional<CategoryDTO> categoryDTO = categoryService.findOne(id);
         return new ResponseEntity(categoryDTO, HttpStatus.OK);
     }
+
+
+    /**
+     * {@code DELETE  /category/:id} : delete the "id" category.
+     *
+     * @param id the id of the categoryDTO to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseMessages> deleteCategory(@PathVariable Long id) {
+        log.debug("REST request to delete Category : {}", id);
+        categoryService.delete(id);
+        return ResponseUtils.response(HttpStatus.OK, DELETED_SUCCESSFULLY);
+    }
+
+
 }
--- src/main/java/com/labs/katandev/controllers/PostController.java
+++ src/main/java/com/labs/katandev/controllers/PostController.java
@@ -1,20 +1,26 @@
 package com.labs.katandev.controllers;
 
+import com.labs.katandev.domain.dto.CategoryDTO;
 import com.labs.katandev.domain.dto.PostDTO;
+import com.labs.katandev.domain.dto.ResponseMessages;
 import com.labs.katandev.domain.entity.Post;
 import com.labs.katandev.exception.BadException;
 import com.labs.katandev.mapper.PostMapper;
 import com.labs.katandev.repository.PostRepository;
 import com.labs.katandev.service.PostService;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
+
+import com.labs.katandev.utils.ResponseUtils;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.util.List;
 
+import static com.labs.katandev.constants.ResponseConstants.DELETED_SUCCESSFULLY;
 
 /**
  * REST controller for managing {@link com.labs.katandev.domain.entity.Post}.
@@ -27,45 +33,66 @@ public class PostController {
 
     private final PostService postService;
 
-    private final PostRepository postRepository;
-
-    private final ModelMapper modelMapper;
-
-    private final PostMapper postMapper;
-
-    public PostController(PostService postService, PostRepository postRepository, ModelMapper modelMapper, PostMapper postMapper) {
+    public PostController(PostService postService) {
         this.postService = postService;
-        this.postRepository = postRepository;
-        this.modelMapper = modelMapper;
-        this.postMapper = postMapper;
     }
 
     /**
      * {@code POST  /posts} : Create a new post.
      *
      * @param postDTO the postDTO to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new postDTO, or with status {@code 400 (Bad Request)} if the post has already an ID.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with
+     *         body the new postDTO, or with status {@code 400 (Bad Request)} if the
+     *         post has already an ID.
      * @throws BadException if the Location URI syntax is incorrect.
      */
     @PostMapping
     public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDTO) throws BadException {
-        log.debug("REST request to save Post : {}", postDTO);
-        if (postDTO.getId() != null) {
-            throw new BadException("Error!");
-        }
-        PostDTO result = postService.save(postDTO);
-        return ResponseEntity.ok(result);
+      log.debug("REST request to save Post : {}", postDTO);
+      if (postDTO.getId() != null) {
+        throw new BadException("Error!");
+      }
+      PostDTO result = postService.save(postDTO);
+      return ResponseEntity.ok(result);
     }
 
     /**
      * {@code GET  /posts} : get all the posts.
      *
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of posts in body.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list
+     *         of posts in body.
      */
     @GetMapping
     public ResponseEntity<List<Post>> findAllPost() {
-        List<Post> post = postService.findAll();
-        return new ResponseEntity<>(post, HttpStatus.OK);
+      List<Post> post = postService.findAll();
+      return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
+  /**
+   * {@code GET  /post/:id} : get the "id" post.
+   *
+   * @param id the id of the postDTO to retrieve.
+   * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the postDTO, or with status {@code 404 (Not Found)}.
+   */
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> findById(@PathVariable Long id) {
+      log.debug("REST request to get Post : {}", id);
+      Optional<PostDTO> post = postService.findOne(id);
+      return new ResponseEntity(post, HttpStatus.OK);
+    }
+
+
+  /**
+   * {@code DELETE  /post/:id} : delete the "id" post.
+   *
+   * @param id the id of the postDTO to delete.
+   * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+   */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseMessages> deletePost(@PathVariable Long id) {
+      log.debug("REST request to delete Category : {}", id);
+      postService.delete(id);
+      return ResponseUtils.response(HttpStatus.OK, DELETED_SUCCESSFULLY);
     }
 
 }
